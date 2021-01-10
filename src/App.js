bd65bc35b059fe422e9d1ec8946419daff819374
Ilona Engblom <ilona.engblom@tuta.io>
2021-01-10 17:35:56 +0200

    starting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React from 'react'
+import { useState } from 'react'
 import './App.css';
 
+import { AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
 function App() {
+
+  const [query, setQuery] = useState('')
+  const [recordings, setRecordings] = useState([])
+
+  const getRecordings = () => {
+    var finalQuery = query.trim()
+    finalQuery = query.replace(' ', '+')
+    // setUrl('https://www.xeno-canto.org/api/2/recordings?query=' + query)
+    var url = 'https://www.xeno-canto.org/api/2/recordings?query=' + finalQuery + '+q:A+year:2021'
+    console.log(url)
+    fetch(url)
+    .then(response => response.json())
+    .then(data => setRecordings(data.recordings))
+  }
+
+  const inputChanged = (event) => {
+    setQuery(event.target.value)
+  }
+
+  const columns = [
+    {
+      field: 'file'
+    },
+    {
+      field: 'date'
+    },
+    {
+      field: 'cnt'
+    },
+    {
+      field: 'type'
+    },
+    {
+      field: 'rmk'
+    }
+
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <input type = "text" name = "query" value = {query} onChange = {inputChanged} />
+      <button onClick = {() => getRecordings()}>Search</button><br />
+      <div className = 'ag-theme-alpine'
+      style = {{height: 560, width: '40%', margin: 'auto'}}>
+        <AgGridReact
+          rowData = {recordings}
+          columnDefs = {columns}
         >
-          Learn React
-        </a>
-      </header>
+
+        </AgGridReact>
+      </div>
     </div>
   );
 }
